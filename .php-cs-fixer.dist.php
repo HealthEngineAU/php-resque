f06d5aa92a3a1b97353b4ab041e94bfcc5ceb749
Thomas Spencer <thomas.spencer@healthengine.com.au>
2023-11-13 17:43:53 +0800

    Revert the bin script

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -6,7 +6,7 @@ use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
 return (new Config())
-    ->setFinder(Finder::create()->in(__DIR__)->name(['resque', 'resque-scheduler']))
+    ->setFinder(Finder::create()->in(__DIR__)->name(['resque']))
     ->setRules([
         '@PER' => true,
     ]);
